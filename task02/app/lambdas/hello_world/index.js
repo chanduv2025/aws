d55c6dc04394168ae1e46e3fc358ba5a7e1ab4af
Vattem Chandra Sekhar Naidu <vattemchandrasekhar_naidu@epam.com>
2025-03-05 14:54:40 +0530

    task02 v2

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -4,17 +4,28 @@ exports.handler = async (event) => {
 
     if (path === "/hello" && method === "GET") {
         return {
-            statusCode: 200,
-            message: 'Hello from Lambda',
-        };
+            "statusCode": 200,
+            "body": {
+              "statusCode": 200,
+              "message": "Hello from Lambda"
+            },
+            "headers": {
+              "content-type": "application/json"
+            },
+            "isBase64Encoded": false
+          }
     }
 
     // Return 400 Bad Request for any other endpoint or method
     return {
-        
-        
-            statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: $P{method}`
-        
-    };
+        "statusCode": 400,
+        "body": {
+          "statusCode": 400,
+          "message": "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+        },
+        "headers": {
+          "content-type": "application/json"
+        },
+        "isBase64Encoded": false
+      }
 };
